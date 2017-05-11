b81ad277e589ca75077951a6ba86f3c96cda1586
Unknown <rostislav.pridal@telia.no>
2017-05-11 14:21:41 +0200

    clean up

--- src/main/java/cz/ssinfotech/chat/Application.java
+++ src/main/java/cz/ssinfotech/chat/Application.java
@@ -3,7 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"com.tieto.chat"})
+@ComponentScan(basePackages = {"cz.ssinfotech.chat"})
 @EnableAutoConfiguration
 public class Application {
 
